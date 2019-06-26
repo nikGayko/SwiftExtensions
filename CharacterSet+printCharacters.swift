b4c5ac1f70a6b414d5eae7776c0ec67a0a052a1f
Nikita Gayko <nik.gayko@gmail.com>
2019-06-26 12:30:29 +0300

    Extension to CharacterSet
    Add function to get all symbols in characterSet as array

---
+++ CharacterSet+printCharacters.swift
@@ -0,0 +1,13 @@
+extension CharacterSet {
+    func allCharacters() -> [Character] {
+        var result: [Character] = []
+        for plane: UInt8 in 0...16 where self.hasMember(inPlane: plane) {
+            for unicode in UInt32(plane) << 16 ..< UInt32(plane + 1) << 16 {
+                if let uniChar = UnicodeScalar(unicode), self.contains(uniChar) {
+                    result.append(Character(uniChar))
+                }
+            }
+        }
+        return result
+    }
+}
